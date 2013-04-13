d9c0b45f95ac0f25cea432c465efe07588e9e460
Sean McQueen <sean@mcqueen.net>
2013-04-12 21:13:03 -0700

    fuck it. twilio account on github it is

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import tokens
 from flask import Flask, request, redirect
 import twilio.twiml
 
+twilio_id = "AC65492579e6a94943a72ebed4c4f4b788"
+twilio_token = "81ebc16c6a6fd61bf25631ee0b649e01"
+
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET', 'POST'])
