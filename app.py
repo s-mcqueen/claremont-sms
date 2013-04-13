25da728007f79777fc8e40573be296e2e4da1d46
Josh Rael <joshuarael@gmail.com>
2013-04-12 21:09:19 -0700

    forgot to capitalize the b

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/", methods = ['GET', 'POST'])
 def hello():
     ''' test '''
-    body = request.values.get('body')
+    body = request.values.get('Body')
     resp = twilio.twiml.Response()
     resp.sms(body)
     return str(resp)
