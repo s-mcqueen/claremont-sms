4d50d376f5f627273e0322cbac6068c346380fd5
Sean McQueen <sean@mcqueen.net>
2013-04-12 21:17:58 -0700

    port 5000

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, redirect
 import twilio.twiml
 
@@ -15,4 +16,5 @@ def hello():
     return str(resp)
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
