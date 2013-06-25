82168f2bcdb4946cd2550bd0f9d892a8ff4005b9
Evan Casey <Evan@Evans-MacBook-Pro-2.local>
2013-06-24 23:14:50 -0700

    reverted back to traditional modal

--- static/js/custom.js
+++ static/js/custom.js
@@ -12,37 +12,15 @@ $(function(){
 
                 if(data.errors) {
                     console.log(data.errors);
-                    var $signup_errors = $('.signup-errors');
-                    $signup_errors.text(data.errors).show();
-                    $("#errors").modal("show");
+
+                    $('.signup-errors').text(data.errors).show();
+                    $("#error").modal("show");
                 }
 
                 else {
                     console.log("success");
 
-                    var flip = flippant.flip;
-                    var flipper = document.getElementById('signup');
-                    var back;
-                    var textarea = '<h1 style="color: #1ABC9C;"> Thanks! </h1>'
-                    var modal = '<h1 style="color: #1ABC9C;"> Thanks! </h1>\
-                    <p>You should receive an SMS confirmation shortly. <p>Send a \
-                    text to your friend by providing their first and last name followed \
-                    by a colon and the message you would like to send. For example:</p> \
-                    <div class="well well-small"><p>Sean McQueen: You are such a dream \
-                    boat! </p></div><p>If your friend has an account with Claremont SMS, \
-                    they will receive an anonymous text to their phone.</p><p>When you\
-                    receive a text, it might look something like this:</p><div class="well\
-                    well-small"><p>You are such a dream boat! (27281)</p></div><p>You have the \
-                    opportunity to guess who sent you that text by responding with the \
-                    five-digit ID number that follows your text. For example:<p><div \
-                    class="well well-small"><p>27281: Evan Casey</p></div><p>If you guess \
-                    correctly, we will let you know!</p>'
-
-                    back = flip(flipper, modal);
-
-                    back.addEventListener('click', function(e){
-                        event.trigger(back, 'close');
-                    })
+                    $("#intro").modal("show");
 
                 }           
         });
