f767a3a47c4a0565eaab2d1efbcec36ae2b3441b
Sean McQueen <sean@mcqueen.net>
2013-07-11 20:39:31 -0700

    final commit before giant merge

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ import pdb
 
 from app import app
 from lib import process
-from models import Message
+# from models import Message
 
 #---------------------------------------------
 # controllers
@@ -19,40 +19,41 @@ from models import Message
 def display():
     ''' displays messages and processes signup form '''
 
-    messages = list(Message.objects())
-    form = forms.SignupForm()
+    # messages = list(Message.objects())
+    # form = forms.SignupForm()
 
-    if form.validate_on_submit():
-        number = "+1" + form.number.data
-        process.requestSignup(number)
-        return redirect("/")
+    # if form.validate_on_submit():
+    #     number = "+1" + form.number.data
+    #     process.requestSignup(number)
+    #     return redirect("/")
 
-    return render_template('index.html', posts = messages, form = form)
+    # return render_template('index.html', posts = messages, form = form)
+    return render_template('index.html')
 
-@app.route("/signup", methods = ['GET','POST'])
-def signup():
-    ''' recieves signup form data via an ajax POST request '''
+# @app.route("/signup", methods = ['GET','POST'])
+# def signup():
+#     ''' recieves signup form data via an ajax POST request '''
 
-    if request.method == "POST":
-        data = request.form
-        signup_str = 'SIGNUP: %s' % data['user']
-        process.processNew(signup_str, data['number'])
-        return jsonify(data)
+#     if request.method == "POST":
+#         data = request.form
+#         signup_str = 'SIGNUP: %s' % data['user']
+#         process.processNew(signup_str, data['number'])
+#         return jsonify(data)
 
     
-@app.route("/receive", methods = ['GET', 'POST'])
-def receive(): 
-    ''' method to recieve texts, parse them, and store in mongo'''
+# @app.route("/receive", methods = ['GET', 'POST'])
+# def receive(): 
+#     ''' method to recieve texts, parse them, and store in mongo'''
 
-	#store the text body and phone number
-    body = request.values.get('Body')
-    number = request.values.get('From')
+# 	#store the text body and phone number
+#     body = request.values.get('Body')
+#     number = request.values.get('From')
     
-    if numberExists(number):
-        process.processExisting(body, number)
+#     if numberExists(number):
+#         process.processExisting(body, number)
 
-    else:
-        process.processNew(body, number)
+#     else:
+#         process.processNew(body, number)
 
 
 #---------------------------------------------
